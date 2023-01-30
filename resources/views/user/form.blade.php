9b119e73cedc9cb146859296d2035db5b08aacaf
CarmenCruces <karmelacfer@gmail.com>
2023-01-30 20:20:55 +0100

    first commit

---
+++ resources/views/user/form.blade.php
@@ -0,0 +1,27 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        {{-- <div class="form-group">
+            {{ Form::label('image') }}
+            {{ Form::text('image', $user->image, ['class' => 'form-control' . ($errors->has('image') ? ' is-invalid' : ''), 'placeholder' => 'Image']) }}
+            {!! $errors->first('image', '<div class="invalid-feedback">:message</div>') !!}
+        </div> --}}
+        <div class="form-group">
+            {{ Form::label('name') }}
+            {{ Form::text('name', $user->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('surname') }}
+            {{ Form::text('surname', $user->surname, ['class' => 'form-control' . ($errors->has('surname') ? ' is-invalid' : ''), 'placeholder' => 'Surname']) }}
+            {!! $errors->first('surname', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('email') }}
+            {{ Form::text('email', $user->email, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => 'Email']) }}
+            {!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
